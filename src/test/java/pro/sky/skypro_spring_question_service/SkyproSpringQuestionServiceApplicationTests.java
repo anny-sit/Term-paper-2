f1d6753e9edbedee9176b110b0060c211c1dc3af
anny-sit <annysit@yandex.ru>
2024-12-06 20:59:24 +0300

    initial commit

---
+++ src/test/java/pro/sky/skypro_spring_question_service/SkyproSpringQuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skypro_spring_question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringQuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
